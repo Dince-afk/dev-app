afe18cd51853a43dfbcefee2c867c00a9fbc9c15
Dino Curic <dino.c@me.com>
2025-10-25 21:44:48 +0200

    feat: add shadcn component demos to components page

--- src/app/(app)/components/page.tsx
+++ src/app/(app)/components/page.tsx
@@ -1,55 +1,21 @@
-"use client";
-
-import {
-  AlertDialog,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+import { BadgeDemo } from "@/components/demos/badges-demo";
+import { AccordionDemo } from "@/components/demos/accordion-demo";
+import { ButtonGroupDemo } from "@/components/demos/button-group-demo";
+import { AvatarDemo } from "@/components/demos/avatar-demo";
+import { AlertDialogDemo } from "@/components/demos/alert-dialog-demo";
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Checkbox } from "@/components/ui/checkbox";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
-import {
-  Item,
-  ItemActions,
-  ItemContent,
-  ItemDescription,
-  ItemTitle,
-} from "@/components/ui/item";
-import { Label } from "@/components/ui/label";
 import { Toggle } from "@/components/ui/toggle";
-import {
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogDescription,
-  AlertDialogTitle,
-} from "@radix-ui/react-alert-dialog";
-import React from "react";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import { ItemDemo } from "@/components/demos/item-demo";
+import { CalendarDemo } from "@/components/demos/calendar-demo";
+import { CardDemo } from "@/components/demos/card-demo";
+import { ComboboxDemo } from "@/components/demos/combobox-demo";
+import { CommandDialogDemo } from "@/components/demos/command";
+import { ContextMenuDemo } from "@/components/demos/context-menu-demo";
+import { DataTableDemo } from "@/components/demos/data-table-demo";
+import { Calendar22 } from "@/components/demos/date-picker-demo";
 
 export default function Page() {
   return (
@@ -62,123 +28,35 @@ export default function Page() {
         <Button variant="link">Link</Button>
         <Button variant="outline">Outline</Button>
       </div>
-
+      <div className="flex gap-4">
+        <Button size="sm">Small</Button>
+        <Button size="default">Default</Button>
+        <Button size="lg">Large</Button>
+        <Button size="icon">Icon</Button>
+      </div>
+      <ButtonGroupDemo />
       <div className="flex flex-col gap-4">
         <Input placeholder="Text" />
         <Input type="email" placeholder="Email" />
         <Input type="file" placeholder="File" />
       </div>
-
       <Toggle>Toggle</Toggle>
-
       <div className="flex gap-3">
         <Checkbox id="checkbox" />
         <Label htmlFor="checkbox">Checkbox</Label>
       </div>
-
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline">Dropdown Menu</Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56" align="start">
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuGroup>
-            <DropdownMenuItem>
-              Profile
-              <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuSub>
-              <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
-              <DropdownMenuPortal>
-                <DropdownMenuSubContent>
-                  <DropdownMenuItem>Email</DropdownMenuItem>
-                  <DropdownMenuItem>Message</DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem>More...</DropdownMenuItem>
-                </DropdownMenuSubContent>
-              </DropdownMenuPortal>
-            </DropdownMenuSub>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>GitHub</DropdownMenuItem>
-          <DropdownMenuItem disabled>API</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            Log out
-            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle>Title</CardTitle>
-          <CardDescription>Description</CardDescription>
-          <CardAction>
-            <Button variant="link">Card Action</Button>
-          </CardAction>
-        </CardHeader>
-        <CardContent>Card Content</CardContent>
-        <CardFooter>Card Footer</CardFooter>
-      </Card>
-
-      <Item variant="outline">
-        <ItemContent>
-          <ItemTitle>Basic Item</ItemTitle>
-          <ItemDescription>
-            A simple item with title and description.
-          </ItemDescription>
-        </ItemContent>
-        <ItemActions>
-          <Button variant="default" size="sm">
-            Action
-          </Button>
-        </ItemActions>
-      </Item>
-
-      <CalendarDemo />
-
+      <ComboboxDemo />
+      <CardDemo />
+      <AvatarDemo />
+      <BadgeDemo />
       <AlertDialogDemo />
+      <ItemDemo />
+      <AccordionDemo />
+      <Calendar22 />
+      <CalendarDemo />
+      <CommandDialogDemo />
+      <ContextMenuDemo />
+      <DataTableDemo />
     </div>
-  );
-}
-
-function CalendarDemo() {
-  const [date, setDate] = React.useState<Date | undefined>(new Date());
-  return (
-    <Calendar
-      mode="single"
-      selected={date}
-      onSelect={setDate}
-      className="rounded-md border shadow-sm"
-      captionLayout="dropdown"
-    />
-  );
-}
-
-function AlertDialogDemo() {
-  return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <Button variant="outline">Show Dialog</Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
   );
 }
